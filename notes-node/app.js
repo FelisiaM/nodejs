7b99b374f09030677ceed1ceeabfbe2a5103914d
Felisia Martini <felisia_martini@hotmail.com>
2017-12-09 18:37:57 +0000

    Using command line arguments as user input

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,15 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('true'));
+var command = process.argv[2]
+console.log('Command: ', command);
+console.log(process.argv);
 
-var filteredArray = _.uniq(['Kelii', '1', 'Feli', '1', '2'])
-console.log(filteredArray)
-// var res = notes.addNote();
-
-// console.log(notes.add(19, 1));
-// var user = os.userInfo();
-
-// fs.appendFileSync('greeting.txt', `Hello ${user.username}! You are ${notes.age}`);
-
+if(command === 'add'){
+    console.log('Adding new note')
+} else if (command === 'list'){
+    console.log('Listing all notes')
+} else if (command === 'read'){
+    console.log('Reading note')
+} else if (command === 'remove'){
+    console.log('Removing note')
+} else {
+    console.log('Command not recognised')
+}
