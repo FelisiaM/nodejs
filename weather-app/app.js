98ca56b704459a39ca50cbf06d6ed14d8d39115d
Felisia Martini <felisia_martini@hotmail.com>
2017-12-17 21:47:32 +0000

    Added address as an argument to be passed to the url

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,21 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch the weather for'
+        }
+    })
+    .help()
+    .argv;
+var address = encodeURIComponent(argv.address);
+
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=edinburgh',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
