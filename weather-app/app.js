187b2cd15631389934ff75b2fb57e530f01c1dea
Felisia Martini <felisia_martini@hotmail.com>
2017-12-17 20:22:07 +0000

    Extracting elements from response obj

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=eh65jj',
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=edinburgh',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));    
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
 });
