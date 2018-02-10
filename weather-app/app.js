826b08ba1cdd9973494efefe71c3047744146ea2
Felisia Martini <felisia_martini@hotmail.com>
2018-02-10 14:09:30 +0000

    moved request to google api to its own file

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -11,14 +12,12 @@ const argv = yargs
     })
     .help()
     .argv;
-var address = encodeURIComponent(argv.address);
 
-
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-    json: true
-}, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2))
+    }
 });
+
